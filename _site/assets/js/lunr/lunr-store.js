ed9251f797bb52e93bea5d6648b877fbe650d788
urpark <urpark@pntbiz.com>
2022-02-16 22:39:23 +0900

    [ramram] springboot redis 적용기 -01

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "[ramram] SpringBoot - Redis Client 적용기",
+        "excerpt":"Spring boot - Lettuce 적용기 들어가며 스프링부트 Redis 적용 방법을 공유하고자 게시물을 작성하였다. 예제코드로 적용 방법을 알아보자 🌟 Redis 의존 설정 Spring은 다양한 Redis 의존설정 방법을 제공한다. 첫번째 방법은 직접 Gradle이나 Maven 설정 파일에 코드를 작성하는 것이다. dependencies { implementation 'org.springframework.boot:spring-boot-starter-data-redis' } 두번째 방법으로는 Intellij tool 을 사용하여 SpringBoot 프로젝트 생성시점에...","categories": ["SpringBoot"],
+        "tags": ["spring","springBoot","redis","lettuce"],
+        "url": "/springboot/spring-boot-redis/",
+        "teaser": null
+      }]
