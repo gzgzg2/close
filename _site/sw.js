aaac787a90aae263ac9b06d651c79407b214ce35
uram <parkuram12@gmail.com>
2022-06-27 19:42:27 +0900

    [blog] feat 블로그 리뉴얼 - 테마 변경

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220627.192832';
+const cacheName = 'chirpy-20220627.194221';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
