c9024f972c875bb6d175ba77ff8321fc644d382e
uram <parkuram12@gmail.com>
2022-06-27 20:01:10 +0900

    [blog] feat 블로그 리뉴얼
    - 테마 변경
    - Gemfile.lock 추가

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220627.194221';
+const cacheName = 'chirpy-20220627.200000';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
